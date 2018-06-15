8587cc610d196f2e0bc0201e07393864a80e31f2
Chemere Hill <chemere27@gmail.com>
2018-06-15 18:26:18 -0400

    updated component

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <div className= "backgroundskew">
+        </div>
         <h1>Bottega Mad Libs</h1>
       </div>
     );
