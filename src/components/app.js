ff4362f825a8006858907aebb668e39730696d6f
Chemere Hill <chemere27@gmail.com>
2018-06-15 20:51:47 -0400

    added header and header styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,15 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <div className= "backgroundskew">
+        <div className= "backgroundSkew">
+        <div className="madlib-heading">
+         <h1>Bottega Mad Libs</h1>
+         <div className=" madlib-subheading">
+          Fill out the feilds below and click generate button<br/> to see the Mad Lib Story
+          </div>
+         </div> 
         </div>
-        <h1>Bottega Mad Libs</h1>
+        
       </div>
     );
   }
